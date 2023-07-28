4478ce5327de3711688888f389acb6870edf0f58
Pappu Lal <73828213+PappuLal1234@users.noreply.github.com>
2023-07-28 16:59:59 +0530

    Update Post.java

--- src/main/java/com/blog/entity/Post.java
+++ src/main/java/com/blog/entity/Post.java
@@ -1,9 +1,6 @@
 package com.blog.entity;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,24 +16,19 @@ import java.util.Set;
 @Entity
 @Table(name="post")
 @JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class,scope=Post.class,property="postId")
-
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     private Integer postId;
     @Column(name="post_title")
     private String postTitle;
     @Column(name="post_content")
-
     private String postContent;
 
     private Date postAddedDate;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name="user_id")
 
-    private User user;
-    @OneToMany(mappedBy = "post",cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL)
     private Set<Comment> comments=new HashSet<>();
 }
+
